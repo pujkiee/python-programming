6d684eb48d74a4ecfe977377c55c16328300eb3b
pujkiee <38775722+pujkiee@users.noreply.github.com>
2018-04-28 09:44:35 +0530

    Update array.py

--- begginer level/array.py
+++ begginer level/array.py
@@ -1,11 +1,10 @@
-from array import *
-N = input()
+n =input()
 my_array =[]
-for i in range (0,N) :
-    x = input()
+for i in range (0,n):
+    x= input()
     my_array.append(x)
-K = input()
-add = 0
-for s in range (0,K):
-    add = add + my_array[s]
-print(add)
+    k=input()
+    add=0
+    for s in range (0,k):
+        add= add+my_array(s)
+        print (add)
