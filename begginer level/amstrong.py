a56ae9a731cf0002af2bb0f87e12cd1d528e24b0
pujkiee <38775722+pujkiee@users.noreply.github.com>
2018-04-30 10:08:52 +0530

    Update amstrong.py

--- begginer level/amstrong.py
+++ begginer level/amstrong.py
@@ -1,15 +1,15 @@
-a = input()
-b = input()
+a=input()
+b=input()
 if(isinstance((a and b),int)):
     for num in range (a,b):
-        temp = num
-        i = temp 
-        power = 0
-        mult = 0
-        while(i > 0):
+        temp=num
+        i=temp
+        power=0
+        mult=0
+        while(i>0):
             power += 1  
             i //= 10
-        while(num > 0):
+        while(num>0):
             rem = num % 10
             mult += pow(rem,power)
             num //= 10
