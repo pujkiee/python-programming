35f033396ba66e965bb949c057c1629b554d75ea
pujkiee <38775722+pujkiee@users.noreply.github.com>
2018-04-30 09:53:03 +0530

    Update prime or not.py

--- begginer level/prime or not.py
+++ begginer level/prime or not.py
@@ -1,9 +1,8 @@
-N = input()
-if(N > 1 and N <= 1000):
-    for i in range(2, N):
-        if (N % i) == 0:
+n=input()
+if(n>1 and n<=1000):
+   for i in range(2,n):
+        if(n%i)==0:
             print("no")
-            break
     else:
         print("yes")
 else:
