cecc0348916f1a278f6ce45280a9c7ea3a89cd9d
pujkiee <38775722+pujkiee@users.noreply.github.com>
2018-04-30 19:43:30 +0530

    Update roman numerals.py

--- player level/roman numerals.py
+++ player level/roman numerals.py
@@ -1,20 +1,27 @@
-def romanMap():
-    map=(("M",  1000),("CM", 900),("D",  500),("CD", 400),("C",  100),("XC", 90),("L",  50),("XL", 40),("X",  10),("IX", 9),("V",  5),("V", 4),("I",  1))
-    return map
-firstNum=ns([0])
-secondNum=ns([1])
-def main():
-    ns=str(input("Enter a roman numeral"))
-    total=0
-    result=0
-    while ns:
-        firstNum=(romanMap(ns[0]))
-         secondNum=(romanMap(ns[1]) 
-        if firstNum is len(ns)>1 or secondNum-1:
-                        total=total+firstNum
-            ns=ns[1:]
-        else:
-                        total=total+ns[1]-ns[0]
-            ns=ns[2:]
-      print (total)
-main()
+from collections import oredredDict
+def write_roman(num):
+    roman= orderedDict()
+    roman[1000]="M"
+    roman[900]="CM"
+    roman[500]="D"
+    roman[400]="CD"
+    roman[100]="C"
+    roman[90]="XC"
+    roman[50]="L"
+    roman[40]="XL"
+    roman[10]="X"
+    roman[9]="IX"
+    roman[5]="V"
+    roman[4]="IV"
+    roman[1]="I"
+    def roman_num(num):
+        for i in roman.keys():
+            x,y=divmod(num,r)
+            yield roman[r]*x
+            num-=(r*x)
+            if num>0:
+                roman_num(num)
+                else:
+                    break
+                    return"".join([a for a in roman_num(num)])
+    
