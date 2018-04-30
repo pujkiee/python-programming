f16562462729692234d6bcfa8a0825461bc372fb
pujkiee <38775722+pujkiee@users.noreply.github.com>
2018-04-30 18:44:46 +0530

    Create reverse.py

---
+++ player level/reverse.py
@@ -0,0 +1,16 @@
+def reverse(1):
+reverse = 1[::-1]
+retuen reverse
+def reverse_in_place(1):
+for i in xrange(len(1)/2):
+beginning=i
+end=-i-1
+temp=1[end]
+1[end]=1[beginning]
+1[beginning]=temp
+return 1
+def reverse_with_fewer_lines(1):
+beginning=i
+end=-i-1
+1[beginning],1[end]=1[end],1[beginning]
+retuen 1
